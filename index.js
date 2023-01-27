399a1b4abf774029f917220712746f623656eefc
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-01-27 21:09:21 +0000

    Adding initial question templates

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions for user
 const questions = [
-
+    {
+        // Boolean choice to begin process
+        name: 'createReadMeChoice',
+        question: 'Begin README.md file generator? (y/n)',
+    },
+    { 
+        name: 'projectTitle', 
+        question: 'Enter project title:' 
+    },
+    {
+        name: 'description',
+        question: 'Enter project description:'
+    },
+    // {
+    //     name: 'tableOfContents',
+    //     question:'?'
+    // },
+    {
+        name: 'installation',
+        question: 'Enter installation intructions:',
+    },
+    { 
+        name: 'usage', 
+        question: 'Enter use instructions:' 
+    },
+    {
+        name: 'license',
+        question: 'Choose a license for your project:',
+    },
+    {
+        name: 'contributing',
+        question: 'Enter contribution section:',
+    },
+    { 
+        name: 'tests', 
+        question: 'Enter tests section text:' 
+    },
+    {
+        name: 'questions',
+        question: 'Enter instructions on how to contact the project admin. for feedback or questions:',
+    },
+    {
+        name: 'githubUserName',
+        question: 'Enter GitHub username:',
+    },
+    {
+        name: 'emailAdress',
+        question: 'Enter email address for your feedback contact:',
+    },
+    // {
+        // Save to a file location
+        // Request path location : "Select location to save the file:"
+    // },
 ];
 
 // function to write README file
