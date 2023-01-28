1f18ae67f79a6e4306e9810d369821ca8e4a3f37
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-01-28 12:04:24 +0000

    Adding inquirer prompt

--- index.js
+++ index.js
@@ -67,7 +67,20 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-    
+    console.log('--------------- README.md File Generator ---------------')
+
+    inquirer.prompt(questions)
+    .then((answers) => {
+         // Use user feedback for... whatever!!
+        console.log(answers);
+    })
+    // .catch((error) => {
+    //     if (error.isTtyError) {
+    //       // Prompt couldn't be rendered in the current environment
+    //     } else {
+    //       // Something else went wrong
+    //     }
+    // });
 }
 
 // function call to initialize program
