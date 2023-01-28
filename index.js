23c22260a268f712ead3e036abc54961be3c8869
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-01-28 20:12:51 +0000

    Inquirer prompts fleshed out  and prompt question types updated.

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const questions = [
     // {
     //     // Boolean choice to begin process
+    //     type:'confirm',
     //     name: 'createReadMeChoice',
-    //     message: 'Begin README.md file generator? (y/n)',
+    //     message: 'Begin README.md file generator?',
     // },
     { 
         type: 'input',
@@ -16,6 +17,7 @@ const questions = [
         message: 'Enter project title:' 
     },
     {
+        type: 'editor',
         name: 'description',
         message: 'Enter project description:'
     },
@@ -24,36 +26,59 @@ const questions = [
     //     message:'?'
     // },
     {
+        type: 'editor',
         name: 'installation',
         message: 'Enter installation intructions:',
     },
     { 
+        type: 'editor',
         name: 'usage', 
         message: 'Enter use instructions:' 
     },
     {
+        type: 'list',
         name: 'license',
         message: 'Choose a license for your project:',
+        choices: [  'None',
+                    'Apache License 2.0', 
+                    'GNU General Public License v3.0', 
+                    'MIT License', 
+                    'BSD 2-Clause "Simplified" License', 
+                    'BSD 3-Clause "New" or "Revised" License', 
+                    'Boost Software License 1.0', 
+                    'Creative Commons Zero v1.0 Universal', 
+                    'Eclipse Public License 2.0', 
+                    'GNU Affero General Public License v3.0', 
+                    'GNU General Public License v2.0', 
+                    'GNU Lesser General Public License v2.1', 
+                    'Mozilla Public License 2.0', 
+                    'The Unlicense'
+                ]
     },
     {
+        type: 'editor',
         name: 'contributing',
         message: 'Enter contribution guidelines:',
     },
     { 
+        type: 'editor',
         name: 'tests', 
         message: 'Enter test instructions:' 
     },
     {
-        name: 'questions',
-        message: 'Enter instructions on how to contact the project admin. for feedback or questions:',
-    },
-    {
+        type: 'input',
         name: 'githubUserName',
         message: 'Enter GitHub username:',
     },
     {
-        name: 'emailAdress',
+        type: 'input',
+        name: 'emailAddress',
         message: 'Enter email address for your feedback contact:',
+    },
+    {
+        type: 'editor',
+        name: 'questions',
+        message: 'Enter instructions on how to contact the project admin. for questions, feedback or to report issues:',
     },
     // {
         // Save to a file location
@@ -67,20 +92,35 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-    console.log('--------------- README.md File Generator ---------------')
+    console.log('\n--------------- README.md File Generator ---------------\n');
 
-    inquirer.prompt(questions)
-    .then((answers) => {
-         // Use user feedback for... whatever!!
-        console.log(answers);
-    })
-    // .catch((error) => {
-    //     if (error.isTtyError) {
-    //       // Prompt couldn't be rendered in the current environment
-    //     } else {
-    //       // Something else went wrong
-    //     }
-    // });
+    // Boolean choice to begin the process of generating the README file
+    inquirer.prompt([
+        {
+            type:'confirm',
+            name: 'createReadMeChoice',
+            message: 'Would you like to initialize the README.md file generator?'
+        }
+    ])
+    .then(answers => {
+        // If the prompt questions
+        if (answers.createReadMeChoice){
+            inquirer.prompt(questions)
+            .then((answers) => {
+                // Use user feedback for... whatever!!
+                console.log(answers);
+            })
+            // .catch((error) => {
+            //     if (error.isTtyError) {
+            //       // Prompt couldn't be rendered in the current environment
+            //     } else {
+            //       // Something else went wrong
+            //     }
+            // });
+            } else {
+                console.log('\n---------------------- Goodbye :) ----------------------\n');
+            }   
+    });
 }
 
 // function call to initialize program
